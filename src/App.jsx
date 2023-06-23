6521dcdb72b3314043ac30e3bc072a4fd9640904
faithdirorimwe <taithdone@gmail.com>
2023-06-23 14:08:56 +0200

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.scss';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import Home from './pages/Home';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+// import home from './pages/home';
+// import Home from './pages/Home';
+// import home from './pages/home'
 
 function App() {
   
